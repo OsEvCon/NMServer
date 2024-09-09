9eabfe0334b604416ed4153612fa5aeae16574e3
evgen <evgeniy.osin.87@mail.ru>
2024-09-10 00:12:34 +0300

    Добавил VisitDao для корректного обновления Visit (были проблемы с @JsonIdentityInfo). I added VisitDao for proper updating of Visit (there were issues with @JsonIdentityInfo).

---
+++ src/main/java/DAO/VisitDao.java
@@ -0,0 +1,62 @@
+package DAO;
+
+import model.Procedure;
+import model.Visit;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class VisitDao {
+    private int visitId;
+    private int clientId;
+    private List<Integer> proceduresId = new ArrayList<>();
+    private LocalDateTime localDateTime;
+
+    public VisitDao(Visit visit){
+        this.visitId = visit.getId();
+        this.clientId = visit.getClient().getId();
+        addProceduresId(visit.getProcedures());
+        this.localDateTime = visit.getVisitDateTime();
+    }
+
+    public VisitDao(){}
+
+    public int getVisitId() {
+        return visitId;
+    }
+
+    public void setVisitId(int visitId) {
+        this.visitId = visitId;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public List<Integer> getProceduresId() {
+        return proceduresId;
+    }
+
+    public void setProceduresId(List<Integer> proceduresId) {
+        this.proceduresId = proceduresId;
+    }
+
+    public void addProceduresId(List<Procedure> procedures){
+        proceduresId.addAll(procedures.stream().map(procedure -> procedure.getId())
+                .collect(Collectors.toList()));
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+}
